1fb3e461b707f53c653c8bcc1037d0c11fab6224
Mc-Nanes <lon@poli.br>
2022-07-31 22:33:48 -0300

    criando as classes e atributos iniciais

---
+++ Celula.java
@@ -0,0 +1,31 @@
+public class Celula {
+    private int x, y;
+    private final boolean celulaVazia;
+    private final String valor;
+    
+    public Celula(int x, int y, String valor, Boolean celulaVazia) {
+        this.x = x;
+        this.y = y;
+        this.valor = valor;
+        this.celulaVazia = celulaVazia;
+    } 
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public String getvalor() {
+        return valor;
+    }
+    public boolean celulaVazia() {
+        return celulaVazia;
+    }
+   
+}
---
+++ Tabuleiro.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Tabuleiro {
+
+    private final int n;
+    private final Celula[][] board;
+    private final ArrayList<Integer> Valores;
+
+}
